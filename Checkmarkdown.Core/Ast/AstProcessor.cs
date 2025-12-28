e1f1be7e5916134b3ea2e25f15e10ef6ee05854e
modo_lv <modo-lv@users.noreply.github.com>
2025-12-29 01:49:13 +0200

    AST processor base.

---
+++ Checkmarkdown.Core/Ast/AstProcessor.cs
@@ -0,0 +1,16 @@
+﻿using Checkmarkdown.Core.Elements.Meta;
+
+namespace Checkmarkdown.Core.Ast;
+
+/// <summary>Base class for components that modify a Checkmarkdown AST in some way.</summary>
+public abstract class AstProcessor {
+    /// <summary>Main functionality for processing and individual element node in the AST.</summary>
+    public abstract Element Process(Element node);
+
+    /// <summary>Runs <see cref="Process"/> on <paramref name="root"/> and all its children.</summary>
+    public virtual Element ProcessRecursively(Element root) {
+        var result = this.Process(root);
+        result.Children = result.Children.Select(this.ProcessRecursively).ToList();
+        return result;
+    }
+}
