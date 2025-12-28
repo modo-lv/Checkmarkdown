33a843863bf3c99543f8133446124268643b4066
modo_lv <modo-lv@users.noreply.github.com>
2025-12-29 01:57:40 +0200

    Document element.

---
+++ Checkmarkdown.Core/Elements/Document.cs
@@ -0,0 +1,6 @@
+﻿using Checkmarkdown.Core.Elements.Meta;
+
+namespace Checkmarkdown.Core.Elements;
+
+/// <summary>Root node of a Markdown/CMD page AST.</summary>
+public class Document : BlockContainer;
