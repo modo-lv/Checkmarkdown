dd883ac18ad16c18959a8983ae020a5e63f3725c
modo_lv <modo-lv@users.noreply.github.com>
2025-12-29 02:29:09 +0200

    Main Markdown->Checkmarkdown class.

---
+++ Checkmarkdown.Core/Ast/FromMarkdown.cs
@@ -0,0 +1,34 @@
+﻿using Checkmarkdown.Core.Elements;
+using Checkmarkdown.Core.Elements.Meta;
+using Checkmarkdown.Core.Markdown;
+using Markdig.Syntax;
+
+namespace Checkmarkdown.Core.Ast;
+
+/// <summary>Handles conversion from Markdown to Checkmarkdown elements.</summary>
+public static class FromMarkdown {
+
+    /// <summary>Parse a Markdown string into a Checkmarkdown AST.</summary>
+    public static Document ToCheckmarkdown(String markdown) {
+        var document = MarkdownParser.ParseToAst(markdown);
+        return (Document) ToCheckmarkdown(document);
+    }
+
+    /// <summary>Convert a Markdown element to a Checkmarkdown one.</summary>
+    public static Element ToCheckmarkdown(IMarkdownObject mdo) {
+        Element result = mdo switch {
+            MarkdownDocument _ => new Document(),
+            _ => new Unrecognized(mdo),
+        };
+
+        var children = mdo switch {
+            LeafBlock node => node.Inline,
+            IEnumerable<MarkdownObject> node => node,
+            _ => null
+        } ?? [];
+
+        result.Children = children.Select(ToCheckmarkdown).ToList();
+
+        return result;
+    }
+}
