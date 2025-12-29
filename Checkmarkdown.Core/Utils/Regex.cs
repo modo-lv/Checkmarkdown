6fd0d2423822b32b622440b9658f6baa7f9388d8
modo_lv <modo-lv@users.noreply.github.com>
2025-12-29 21:26:28 +0200

    Utils: Whitespace regex.

---
+++ Checkmarkdown.Core/Utils/Regex.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Checkmarkdown.Core.Utils;
+
+public static partial class RegexPatterns {
+    [GeneratedRegex(@"\s")]
+    public static partial Regex WhiteSpace { get; }
+}
