1372022a38446aedbf218df1452cd4c5cf37c61c
modo_lv <modo-lv@users.noreply.github.com>
2025-12-29 19:17:35 +0200

    Collection utils: * Find index extension for read-only lists. * Aliases for string-key maps.

--- Checkmarkdown.Core/Utils/CollectionUtils.cs
+++ Checkmarkdown.Core/Utils/CollectionUtils.cs
@@ -1,8 +1,34 @@
 ﻿namespace Checkmarkdown.Core.Utils;
 
+/// <summary>More readable shorthand for common string-key dictionaries.</summary>
+public interface IStringMap<T> : IDictionary<String, T>;
+
+/// <summary>More readable shorthand for common string-key dictionaries.</summary>
+public class StringMap<T> : Dictionary<String, T>, IStringMap<T>;
+
+/// <summary>More readable shorthand for common string-string dictionaries.</summary>
+public interface IKeyValueMap : IDictionary<String, String>;
+
+/// <summary>More readable shorthand for common string-string dictionaries.</summary>
+public class KeyValueMap : Dictionary<String, String>, IKeyValueMap;
+
 public static class CollectionUtils {
     extension(Object target) {
         public Boolean IsIn(params Object[] candidates) =>
             candidates.Contains(target);
+    }
+
+    extension<T>(IReadOnlyList<T> list) {
+        /// <summary>
+        /// Finds the index of the first item in the list matching <paramref name="predicate"/>.
+        /// </summary>
+        /// <returns>Index of item, or <c>null</c> if not found.</returns>
+        public Int32? FindIndex(Predicate<T> predicate) {
+            for (var i = 0; i < list.Count; i++) {
+                if (predicate(list[i])) 
+                    return i;
+            }
+            return null;
+        }
     }
 }
