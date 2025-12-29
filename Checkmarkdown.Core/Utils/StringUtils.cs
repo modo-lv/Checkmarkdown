6fe682071b9f28fb1273422c690b559d7383c081
modo_lv <modo-lv@users.noreply.github.com>
2025-12-29 20:55:36 +0200

    Utils: "or empty" extension for nullable strings.

---
+++ Checkmarkdown.Core/Utils/StringUtils.cs
@@ -0,0 +1,9 @@
+﻿namespace Checkmarkdown.Core.Utils;
+
+public static class StringUtils {
+    extension(String? value) {
+        /// <summary>Fluent alternative to <c>value ?? ""</c>.</summary>
+        public String OrEmpty() =>
+            value ?? "";
+    }
+}
