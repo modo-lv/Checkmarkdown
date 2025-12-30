fd6c23b9d6aebc8825e9640dd5e89da0e14604d7
modo_lv <modo-lv@users.noreply.github.com>
2025-12-31 00:37:07 +0200

    Explicit title-based ID support.

---
+++ Checkmarkdown.Core/Ast/Processors/TitleIdProcessor.cs
@@ -0,0 +1,15 @@
+﻿using Checkmarkdown.Core.Elements.Meta;
+
+namespace Checkmarkdown.Core.Ast.Processors;
+
+/// <summary>
+/// Markdig understands <c>{#id}</c> syntax and we set the ID from that if present,
+/// but we also want to support generating the ID from title with <c>{#:id}</c>.
+/// </summary>
+public class TitleIdProcessor : AstProcessor {
+    public override Element Process(Element node) {
+        if (node.ExplicitId == ":id")
+            node.ExplicitId = Globals.Id(node.TitleText);
+        return node;
+    }
+}
---
+++ Checkmarkdown.Core/Globals.cs
@@ -0,0 +1,21 @@
+﻿using Checkmarkdown.Core.Utils;
+
+namespace Checkmarkdown.Core;
+
+public static class Globals {
+    /// <summary>
+    /// Prefix to when Forkdown-specific element attributes share a space with user-provided ones
+    /// </summary>
+    public const String Prefix = "fd--";
+
+    /// <summary>Convert a string to a valid global ID.</summary>
+    /// <param name="input">String to convert.</param>
+    /// <returns>A syntactically valid global ID.</returns>
+    public static String Id(String? input) {
+        return input.OrEmpty().Trim()
+            .Let(it => RegexPatterns.WhiteSpace.Replace(it, "_"))
+            .Replace("'", "_")
+            .Replace("\"", "_")
+            .ToLowerInvariant();
+    }
+}
