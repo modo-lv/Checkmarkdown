526f76d12201d71ee709d6dc4987b2db555b282e
modo_lv <modo-lv@users.noreply.github.com>
2025-12-31 00:36:39 +0200

    Document attributes.

---
+++ Checkmarkdown.Core/Ast/Processors/DocumentAttributeProcessor.cs
@@ -0,0 +1,23 @@
+﻿using Checkmarkdown.Core.Elements;
+using Checkmarkdown.Core.Elements.Meta;
+
+namespace Checkmarkdown.Core.Ast.Processors;
+
+/// <remarks>
+/// Setting attributes on the whole document (not just its elements) is done by starting the document with
+/// ":{..attributes..}". Markdig parses this as a paragraph with the attributes, so we move the attributes
+/// to the document.
+/// </remarks>
+public class DocumentAttributeProcessor : AstProcessor {
+    public override Element Process(Element node) {
+        if (node is not Document) return node;
+        
+        if (node.Children.FirstOrDefault() is Paragraph p &&
+            p.Children.FirstOrDefault() is Text { Content: ":" }) {
+            p.MoveAttributesTo(node);
+            node.Children.Remove(p);
+        }
+        
+        return node;
+    }
+}
