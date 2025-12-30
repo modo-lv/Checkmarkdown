8de1f4dee45c804f494985961f1c886696f5090e
modo_lv <modo-lv@users.noreply.github.com>
2025-12-31 01:47:43 +0200

    Add file path and depth to documents.

--- Checkmarkdown.Core/Utils/StringUtils.cs
+++ Checkmarkdown.Core/Utils/StringUtils.cs
@@ -5,5 +5,14 @@ public static class StringUtils {
         /// <summary>Fluent alternative to <c>value ?? ""</c>.</summary>
         public String OrEmpty() =>
             value ?? "";
+        
+    }
+
+    extension(String value) {
+        public String TrimPrefix(String prefix) =>
+            value.StartsWith(prefix) ? value[prefix.Length..] : value;
+        
+        public String TrimSuffix(String suffix) =>
+            value.EndsWith(suffix) ? value[..^suffix.Length] : value;
     }
 }
