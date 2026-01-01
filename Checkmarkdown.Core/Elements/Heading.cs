150a4b1a233c48c59b786b1095e2bb4cf0db471b
modo_lv <modo-lv@users.noreply.github.com>
2026-01-01 12:08:06 +0200

    Heading element.

---
+++ Checkmarkdown.Core/Elements/Heading.cs
@@ -0,0 +1,8 @@
+﻿using Checkmarkdown.Core.Elements.Meta;
+using MarkdigHeading = Markdig.Syntax.HeadingBlock;
+
+namespace Checkmarkdown.Core.Elements; 
+
+public class Heading(MarkdigHeading mHeading) : Block(mHeading) {
+  public readonly Int32 Level = mHeading.Level;
+}
