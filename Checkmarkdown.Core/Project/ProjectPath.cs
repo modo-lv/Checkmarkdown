f8bc3a2d1cad7bb68b2a3c22563a3a6e503d58db
modo_lv <modo-lv@users.noreply.github.com>
2026-01-13 01:24:36 +0200

    Add project path class.

---
+++ Checkmarkdown.Core/Project/ProjectPath.cs
@@ -0,0 +1,39 @@
+﻿using Path = Fluent.IO.Path;
+
+namespace Checkmarkdown.Core.Project;
+
+/// <summary>Provides convenient reference to a project file path.</summary>
+public class ProjectPath {
+    /// <summary>Project root path.</summary>
+    public readonly Path Root;
+
+    /// <summary>File path relative to project root.</summary>
+    public readonly Path Relative;
+
+    /// <summary>Full file path, including project root.</summary>
+    public readonly Path Full;
+
+    /// <summary>Does this path exist in the filesystem?</summary>
+    public Boolean Exists => Full.Exists;
+
+    public String RelPathString {
+        get => field;
+        private init => field = Relative.ToString().Replace('\\', '/');
+    }
+
+    public ProjectPath(Path root, Path relativeFilePath) {
+        Root = root;
+        Relative = relativeFilePath.IsRooted ? relativeFilePath.MakeRelativeTo(root) : relativeFilePath;
+        Full = this.Root.Combine(this.Relative);
+    }
+
+
+
+    public override String ToString() => RelPathString;
+    public String FullPathString() => Full.FullPath.Replace('\\', '/');
+
+    public IEnumerable<ProjectPath> Files(String filter, Boolean recursive) =>
+        Full.Files(filter, recursive).Select(it => new ProjectPath(Root, it));
+
+    public Path Combine(String path) => Full.Combine(path);
+}
