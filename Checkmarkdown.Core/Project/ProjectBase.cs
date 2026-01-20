63044db2b4383fbe6ad5f68e53cdf4103e470b83
modo_lv <modo-lv@users.noreply.github.com>
2026-01-21 00:49:24 +0200

    [core] Add project base class.

---
+++ Checkmarkdown.Core/Project/ProjectBase.cs
@@ -0,0 +1,15 @@
+﻿using Path = Fluent.IO.Path;
+
+namespace Checkmarkdown.Core.Project;
+
+/// <summary>Base class for Checkmarkdown projects.</summary>
+public abstract class ProjectBase(Path rootPath)
+{
+    public Path RootPath = rootPath;
+
+    public ProjectPath PathTo(String file) =>
+        this.PathTo(new Path(file));
+
+    public ProjectPath PathTo(Path file) =>
+        new ProjectPath(this.RootPath, file);
+}
