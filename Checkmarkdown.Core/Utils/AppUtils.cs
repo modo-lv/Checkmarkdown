ef1a98cb107559641794c13af23f1b6af105b466
modo_lv <modo-lv@users.noreply.github.com>
2026-01-28 16:59:24 +0200

    [core] Add fatal exit util.

---
+++ Checkmarkdown.Core/Utils/AppUtils.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+
+namespace Checkmarkdown.Core.Utils;
+
+public static class AppUtils
+{
+    private static readonly TimeSpan _exitDelay = TimeSpan.FromSeconds(5);
+    /**
+     * Log a fatal error message and exit the application.
+     */
+    public static void FatalExit(String message, params Object[] vars) {
+#pragma warning disable CA2254 // Template should be a static expression
+        Log.Fatal(message, vars);
+#pragma warning restore CA2254 // Template should be a static expression
+        Console.WriteLine(
+            $"Fatal error encountered (see above), exiting in {_exitDelay.Seconds} second(s)..."
+        );
+        Thread.Sleep(_exitDelay);
+        Environment.Exit(1);
+    }
+}
